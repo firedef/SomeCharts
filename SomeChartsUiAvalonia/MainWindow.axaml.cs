3c9864b5393cc7d7e097cc0f68f9178623ff29d0
firedef <firedef2019@gmail.com>
2022-01-27 11:54:03 +0300

    init

---
+++ SomeChartsUiAvalonia/MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SomeChartsUiAvalonia {
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		#if DEBUG
+			this.AttachDevTools();
+		#endif
+		}
+
+		private void InitializeComponent() {
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
