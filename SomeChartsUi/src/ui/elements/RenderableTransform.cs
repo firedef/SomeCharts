8aacb0ec253d87d3e81cfb54c5f6957de6a7cdf9
firedef <firedef2019@gmail.com>
2022-02-08 21:31:23 +0300

    added different transform space support

--- SomeChartsUi/src/ui/elements/RenderableTransform.cs
+++ SomeChartsUi/src/ui/elements/RenderableTransform.cs
@@ -6,16 +6,30 @@ public class RenderableTransform {
 	public float2 position;
 	public float2 scale;
 	public float3 rotation;
+	public TransformType type;
 
-	public RenderableTransform(float2 position, float2 scale, float3 rotation) {
+	public RenderableTransform(float2 position, float2 scale, float3 rotation, TransformType type = TransformType.worldSpace) {
 		this.position = position;
 		this.scale = scale;
 		this.rotation = rotation;
+		this.type = type;
 	}
 	
-	public RenderableTransform(float2 position, float scale = 1, float rotation = 0) {
+	public RenderableTransform(float2 position, float scale = 1, float rotation = 0, TransformType type = TransformType.worldSpace) {
 		this.position = position;
 		this.scale = scale;
 		this.rotation = rotation;
+		this.type = type;
 	}
+}
+
+public enum TransformType : byte {
+	/// coordinates from -∞ to +∞
+	worldSpace = 0, 
+	
+	/// coordinates from 0 to screen width or height
+	screenSpace = 1, 
+	
+	/// coordinates from 0 to 1
+	viewportSpace = 2,
 }
--- SomeChartsUiAvalonia/src/backends/SkiaChartsBackend.cs
+++ SomeChartsUiAvalonia/src/backends/SkiaChartsBackend.cs
@@ -5,10 +5,8 @@ using SkiaSharp;
 using SkiaSharp.HarfBuzz;
 using SomeChartsUi.backends;
 using SomeChartsUi.themes.colors;
-using SomeChartsUi.ui.canvas;
 using SomeChartsUi.ui.elements;
 using SomeChartsUi.ui.text;
-using SomeChartsUi.utils;
 using SomeChartsUi.utils.rects;
 using SomeChartsUi.utils.vectors;
 using SomeChartsUiAvalonia.utils;
@@ -18,20 +16,37 @@ namespace SomeChartsUiAvalonia.backends;
 public class SkiaChartsBackend : ChartsBackendBase, IDisposable {
 	private SKCanvas? _canvas;
 	private SKPaint? _paint;
+
+	//TODO: add 3D rotation
+	private void ApplyTransform(RenderableTransform transform, bool flipY = false) {
+		float2 scale = transform.scale;
+		float2 position = transform.position;
+		float3 rotation = transform.rotation;
+
+		// apply canvas transform if world-space 
+		if (transform.type == TransformType.worldSpace) {
+			_canvas!.RotateRadians(owner.transform.rotation.animatedValue.z);
+			_canvas.Scale(owner.transform.zoom.animatedValue.sk());
+			_canvas.Translate(owner.transform.position.animatedValue.sk());
+		}
+		
+		// normalize coordinates if viewport-space
+		if (transform.type == TransformType.viewportSpace) {
+			scale /= owner.transform.screenBounds.widthHeight;
+			position -= owner.transform.screenBounds.leftBottom;
+		}
+		
+		if (flipY) position.FlipY();
+		
+		// apply transform
+		_canvas!.Scale(scale.sk());
+		_canvas.Translate(position.sk());
+		_canvas.RotateRadians(rotation.z);
+	}
 	
 	public override unsafe void DrawMesh(float2* points, float2* uvs, color* colors, ushort* indexes, int vertexCount, int indexCount, RenderableTransform transform) {
 		_canvas!.Save();
-
-		//TODO: add 3D rotation
-		_canvas.Scale(transform.scale.sk());
-		_canvas.Translate(transform.position.sk());
-		//_canvas.Rotate3D(transform.rotation + owner.transform.rotation.animatedValue);
-		
-		// _canvas.Transform(transform.position,
-		//                   transform.scale,
-		//                   transform.rotation + owner.transform.rotation.animatedValue);
-		
-		_canvas.RotateRadians(transform.rotation.z);
+		ApplyTransform(transform);
 		_canvas.DrawVerticesUnsafe(SKBlendMode.Modulate, (SKPoint*)points, (SKPoint*)uvs, (SKColor*)colors, indexes, vertexCount, indexCount, _paint!);
 		_canvas.Restore();
 	}
@@ -42,28 +57,26 @@ public class SkiaChartsBackend : ChartsBackendBase, IDisposable {
 					fixed(ushort* indexesPtr = indexes)
 						DrawMesh(pointsPtr, uvsPtr, colorsPtr, indexesPtr, points.Length, indexes.Length, transform);
 	}
+	
 	public override void DrawText(string text, color col, FontData font, RenderableTransform transform) {
 		if (string.IsNullOrEmpty(text)) return;
 		
 		// flip y axis of canvas back, so text will render not upside-down
 		_canvas!.Save();
 		_canvas!.Scale(1,-1);
-		float2 pos = transform.position;
-		pos.FlipY();
+		ApplyTransform(transform, true);
 
 		_paint!.Color = col.sk();
 		_paint.TextSize = transform.scale.x;
 		_paint.TextScaleX = transform.scale.y / transform.scale.x;
 		
-		
 		_canvas.RotateRadians(transform.rotation.z);
-		_canvas.DrawShapedText(SkiaFontFamilies.Get(font), text, pos.sk(), _paint);
+		_canvas.DrawShapedText(SkiaFontFamilies.Get(font), text, SKPoint.Empty, _paint);
 		_canvas.Restore();
 	}
 	public override void DrawRect(rect rectangle, color color) {
 		_paint!.Color = color.sk();
 		_canvas!.Save();
-		// _canvas!.Scale(1,-1);
 		_canvas!.DrawRect(rectangle.sk(), _paint);
 		_canvas.Restore();
 	}
@@ -75,10 +88,6 @@ public class SkiaChartsBackend : ChartsBackendBase, IDisposable {
 		canvas.Scale(1,-1);
 		canvas.Translate(0, -owner.transform.screenBounds.height);
 		
-		canvas.RotateRadians(owner.transform.rotation.animatedValue.z);
-		canvas.Scale(owner.transform.zoom.animatedValue.sk());
-		canvas.Translate(owner.transform.position.animatedValue.sk());
-
 		_canvas = canvas;
 		_paint ??= new();
 		
